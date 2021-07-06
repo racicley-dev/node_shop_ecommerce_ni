974a4ecdc351b57ce64410161107406799303bb0
Thiago da Costa Pinto Frazão de Sousa <f514189@corp.caixa.gov.br>
2021-07-05 21:47:32 -0300

    estrutura inicial do projeto

--- README.md
+++
@@ -1,2 +0,0 @@
-# node_shop_ecommerce_ni
-Repositório destinado a estudos de Node.js, Ionic, Angular e JS.
---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
